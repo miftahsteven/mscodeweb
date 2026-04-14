5ad95ed5a54fd79bd927c207ed5a7923499a5eba
Miftah Syarief <miftahsyarief@mscode-lab.local>
2026-04-14 15:04:51 +0700

    tambahan untuk analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -141,9 +141,9 @@ export default function RootLayout({
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-WZHFV5Q420"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="beforeInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
